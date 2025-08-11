7502939ba3288761d8a3a93dd141d678f19326da
madhav-k <madhavkarthickk.1212@gmail.com>
2025-08-11 12:09:48 +0530

    ok

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,8 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname)
 
-module.exports = withNativeWind(config, { input: './globals.css' })
+// Extract the string value into a reusable constant
+const GLOBAL_CSS_PATH = "./app/globals.css";
+
+
+module.exports = withNativeWind(config,{input: "GLOBAL_CSS_PATH"})
